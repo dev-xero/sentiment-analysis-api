98aaeeda023b69af0882e6875eae304a12ac396f
dev-xero <elo48858@gmail.com>
2024-04-26 13:30:49 +0100

    feat: Implement proto analyze endpoint and error handlers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,37 @@ def home():
         "code": 200
     }
     return jsonify(res)
+
+
+@app.route("/analyze", methods=['POST'])
+def analyze():
+    if request.method == 'POST':
+        res = {
+            "message": "Analyzed Sentiment.",
+            "success": True,
+            "code": 200
+        }
+        return jsonify(res)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    res = {
+        "message": "Invalid endpoint accessed.",
+        "success": False,
+        "code": 404
+    }
+    return jsonify(res), 404
+
+
+@app.errorhandler(405)
+def not_found(e):
+    res = {
+        "message": "Request method not allowed.",
+        "success": False,
+        "code": 405
+    }
+    return jsonify(res), 405
 
 
 if __name__ == "__main__":
